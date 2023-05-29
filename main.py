70f9b4ee6a591787ec5b7e1a3e543c2bc9c315c3
canonrock16 <canonrock0401@gmail.com>
2023-05-29 16:18:14 +0900

    got it working for now

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 # %%
+
+import lightgbm as lgb
 import numpy as np
 import pandas as pd
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
 
 # %%
 # area_1148 = pd.read_csv("./catches_area_1148_from_20230527_to_20200501.csv_with_details.csv")
@@ -108,12 +113,61 @@ X_df
 
 # %%
 input_df = pd.merge(X_df, Y_df[["date", "area_name", "fish_name", "fish_per_user"]], on=["date", "area_name", "fish_name"], how="left")
+input_df = input_df.drop(columns=["date"])
+
+
+# %%
+
+categorical_columns = ["area_name", "fish_name", "wind_direction", "tide_name", "weather"]
+# Convert categorical columns to 'category' data type
+# for col in categorical_columns:
+# input_df[col] = input_df[col].astype("category")
+
+# Create a LabelEncoder for each categorical column
+encoders = {col: LabelEncoder() for col in categorical_columns}
+# fit and transform each encoder to the corresponding column
+for col, encoder in encoders.items():
+    input_df[col] = encoder.fit_transform(input_df[col])
+
+# %%
 input_df
-
-
+# %%
+# create a train/test split
+X_train, X_test, y_train, y_test = train_test_split(
+    input_df.drop(columns=["fish_per_user"]), input_df["fish_per_user"], test_size=0.2, random_state=42
+)
 # %%
 
+# create dataset for lightgbm
+lgb_train = lgb.Dataset(X_train, y_train, categorical_feature=categorical_columns)
+lgb_eval = lgb.Dataset(X_test, y_test, reference=lgb_train, categorical_feature=categorical_columns)
 # %%
 
+# specify your configurations as a dict
+params = {
+    "boosting_type": "gbdt",
+    "objective": "regression",
+    "metric": {"l2", "l1"},
+    "num_leaves": 31,
+    "learning_rate": 0.05,
+    "feature_fraction": 0.9,
+    "bagging_fraction": 0.8,
+    "bagging_freq": 5,
+    "verbose": 0,
+}
+
+print("Starting training...")
+# train
+gbm = lgb.train(params, lgb_train, num_boost_round=20, valid_sets=lgb_eval, early_stopping_rounds=5)
+
+print("Saving model...")
+# save model to file
+gbm.save_model("model.txt")
+
+print("Starting predicting...")
+# predict
+y_pred = gbm.predict(X_test, num_iteration=gbm.best_iteration)
+# eval
+print("The rmse of prediction is:", mean_squared_error(y_test, y_pred) ** 0.5)
 
 # %%
